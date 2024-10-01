4e748c698a7ad75f4bde28a59aff4574395f4247
Donovan <donovan.courtney@gmail.com>
2024-10-01 13:45:27 -0500

    styled and animated navbarMenu

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -5,26 +5,54 @@ import {
   Navbar,
   NavbarBrand,
   NavbarContent,
+  NavbarItem,
   NavbarMenu,
   NavbarMenuItem,
   NavbarMenuToggle,
 } from "@nextui-org/react";
-import { useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { init } from "next/dist/compiled/webpack/webpack";
 
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const handleClose = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const containerVariants = {
+    variants: {
+      enter: {
+        transformOrigin: "top",
+        // scaleY: 1,
+        opacity: 1,
+        transition: { staggerChildren: 0.1 },
+      },
+      exit: {
+        transformOrigin: "top",
+        // scaleY: 0,
+        opacity: 0,
+        transition: {delay: 0.3, staggerChildren: 0.1 },
+      },
+    },
+  };
+
+  const itemVariants = {
+    enter: { opacity: 1, y: 0, transition: { duration: 0.1 } },
+    exit: { opacity: 0, y: -20, transition: { duration: 0.1 } },
+  };
+
   return (
     <Navbar
+      // className="h-[5rem]"
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
       maxWidth="xl"
       height="5rem"
       shouldHideOnScroll
+      isBlurred={false}
     >
       {/* Center the Brand */}
       <NavbarContent justify="center" className="md:hidden">
@@ -44,46 +72,68 @@ const NavBar = () => {
 
       {/* Navbar Links */}
       <NavbarContent justify="end" className="hidden md:flex space-x-8">
-        <Link
-          href="/"
-          className="hover:text-indigo-400 transition duration-200"
-        >
-          <p>Home</p>
-        </Link>
-        <Link
-          href="/gallery"
-          className="hover:text-indigo-400 transition duration-200"
-        >
-          <p>Gallery</p>
-        </Link>
-        <Link
-          href="/contact"
-          className="hover:text-indigo-400 transition duration-200"
-        >
-          <p>Contact</p>
-        </Link>
+        <NavbarItem>
+          <Link
+            href="/"
+            className="hover:text-indigo-400 transition duration-200"
+          >
+            <p>Home</p>
+          </Link>
+        </NavbarItem>
+
+        <NavbarItem>
+          <Link
+            href="/gallery"
+            className="hover:text-indigo-400 transition duration-200"
+          >
+            <p>Gallery</p>
+          </Link>
+        </NavbarItem>
+
+        <NavbarItem>
+          <Link
+            href="/contact"
+            className="hover:text-indigo-400 transition duration-200"
+          >
+            <p>Contact</p>
+          </Link>
+        </NavbarItem>
       </NavbarContent>
 
       {/* Mobile Menu */}
-      <NavbarMenu>
-        <NavbarMenuItem>
-          <Link href="/" onPress={handleClose}>
-            <p className="text-white hover:text-indigo-400 transition">Home</p>
-          </Link>
-          <Link href="/gallery" onPress={handleClose}>
-            <p className="text-white hover:text-indigo-400 transition">
-              Gallery
-            </p>
-          </Link>
-          <Link href="/about" onPress={handleClose}>
-            <p className="text-white hover:text-indigo-400 transition">About</p>
-          </Link>
-          <Link href="/contact" onPress={handleClose}>
-            <p className="text-white hover:text-indigo-400 transition">
-              Contact
-            </p>
-          </Link>
-        </NavbarMenuItem>
+      <NavbarMenu
+        className="flex-col justify-center items-center pb-[8rem] gap-y-12"
+        motionProps={containerVariants}
+      >
+        <motion.div variants={itemVariants}>
+          <NavbarMenuItem>
+            <Link href="/" onPress={handleClose}>
+              <p className="text-white text-5xl hover:text-indigo-400 transition">
+                Home
+              </p>
+            </Link>
+          </NavbarMenuItem>
+        </motion.div>
+
+        <motion.div variants={itemVariants}>
+          <NavbarMenuItem as={motion.div}>
+            <Link href="/gallery" onPress={handleClose}>
+              <p className="text-white text-5xl hover:text-indigo-400 transition">
+                Gallery
+              </p>
+            </Link>
+          </NavbarMenuItem>
+        </motion.div>
+
+        <motion.div variants={itemVariants}>
+          <NavbarMenuItem as={motion.div} variants={itemVariants}>
+            <Link href="/contact" onPress={handleClose}>
+              <p className="text-white text-5xl hover:text-indigo-400 transition">
+                Contact
+              </p>
+            </Link>
+          </NavbarMenuItem>
+        </motion.div>
       </NavbarMenu>
     </Navbar>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -12,27 +12,25 @@ const LandingPage = () => {
   const circle2Ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // GSAP animation for the first circle (moving clockwise)
     gsap.to(circle1Ref.current, {
       duration: 60,
-      repeat: -1, // Infinite looping
+      repeat: -1,
       ease: "linear",
       motionPath: {
         alignOrigin: [0.5, 0.5],
-        path: "M 0, 0 A 500,500 0 1,1 0.1,0 Z", // Clockwise motion starting at (0,0)
-        autoRotate: false, // Prevent the object itself from rotating
-        curviness: 1.25, // Adjust how curved the path is
+        path: "M 0, 0 A 500,500 0 1,1 0.1,0 Z",
+        autoRotate: false,
+        curviness: 1.25, 
       },
     });
 
-    // GSAP animation for the second circle (moving counterclockwise)
     gsap.to(circle2Ref.current, {
       duration: 60,
       repeat: -1,
       ease: "linear",
       motionPath: {
         alignOrigin: [0.5, 0.5],
-        path: "M 0, 0 A -400,-400 0 1,1 -0.1,0 Z", // Counterclockwise motion starting at (0,0)
+        path: "M 0, 0 A -400,-400 0 1,1 -0.1,0 Z",
         autoRotate: false,
         curviness: 1.25,
       },
@@ -40,36 +38,36 @@ const LandingPage = () => {
   }, []);
 
  return (
-   <div className="min-h-screen overflow-hidden bg-gray-900 text-white">
+   <div className="min-h-[calc(100dvh-5rem)] h-[calc(100dvh-5rem)] overflow-hidden bg-gray-900 text-white">
      {/* Hero Section */}
-     <section className="relative h-screen flex flex-col items-center justify-center text-center px-4">
-       <div>
-         <h1 className="text-4xl font-bold">
-           Art Interpreting the Pulse of Our World
-         </h1>
-         <p className="mt-4 text-lg opacity-75 max-w-xl">
-           AI-generated visuals interpreting current events, news, and popular
-           culture. Dive into a unique blend of art and technology.
-         </p>
+     <section className="relative flex flex-col items-center justify-center h-full px-4 text-center">
+       {/* <div className="mb-10"> */}
+       <h1 className="text-4xl font-bold">
+         Art Interpreting the Pulse of Our World
+       </h1>
+       <p className="mt-4 text-lg opacity-75 max-w-xl">
+         AI-generated visuals interpreting current events, news, and popular
+         culture. Dive into a unique blend of art and technology.
+       </p>
 
-         {/* Call to Action - Button to Gallery */}
-         <a
-           href="/gallery"
-           className="mt-8 inline-block px-8 py-4 bg-indigo-600 text-white rounded-md text-lg tracking-wide hover:bg-indigo-500 transition-transform duration-200 transform hover:scale-105"
-         >
-           Explore the Gallery
-         </a>
-       </div>
+       {/* Call to Action - Button to Gallery */}
+       <a
+         href="/gallery"
+         className="mt-8 inline-block px-8 py-4 bg-indigo-600 text-white rounded-md text-lg tracking-wide hover:bg-indigo-500 transition-transform duration-200 transform hover:scale-105"
+       >
+         Explore the Gallery
+       </a>
+       {/* </div> */}
 
        {/* Abstract Background Shapes with GSAP Circular Motion */}
-       <div
+       {/* <div
          ref={circle1Ref}
          className="absolute top-1/2 left-[20%] w-[500px] h-[500px] bg-indigo-400 opacity-10 rounded-full blur-3xl"
        ></div>
        <div
          ref={circle2Ref}
          className="absolute top-0 right-[20%] w-[300px] h-[300px] bg-red-400 opacity-10 rounded-full blur-3xl"
-       ></div>
+       ></div> */}
      </section>
    </div>
  );
