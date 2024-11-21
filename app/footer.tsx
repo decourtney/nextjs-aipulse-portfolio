6b2c104d9a1feade7c44b4dc00a4a6e7c8e21eb8
Donovan <donovan.courtney@gmail.com>
2024-11-21 14:15:57 -0600

    added linkedin icon to footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,9 +1,25 @@
+import { Link } from "@nextui-org/react";
+
 const Footer = () => {
   return (
-    <footer className="py-2 lg:py-8 bg-foreground text-center text-content1">
-      <p className="text-sm opacity-70">
-        © 2024 Donovan Courtney. All rights reserved.
-      </p>
+    <footer className="w-full h-full bg-foreground px-4 py-4">
+      <Link
+        href="https://www.linkedin.com/in/decourtney/"
+        className="w-full h-[32px] mb-2"
+        target="_blank"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 50 50"
+          className="w-full h-full fill-content1"
+        >
+          <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z" />
+        </svg>
+      </Link>
+
+      <div className="text-center text-sm opacity-70 text-content1">
+        <p>© 2024 Donovan Courtney. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
